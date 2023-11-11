1fa99751c976280b9edc7b47f7a95a73ff723f07
Hilcbek <balemayehu07@gmail.com>
2023-11-11 20:20:17 +0300

    added back

--- Routers/postRouter.js
+++ Routers/postRouter.js
@@ -1,12 +1,20 @@
-import express from 'express'
-import { VerifyLoggedInUser } from '../auth/token.js'
-import { AddPost, DeletePost, FetchPost, LoggedInUserRelatedPost, RelatedUserPost, SinglePost, editPost } from '../Controllers/post.Controller.js'
+import express from "express";
+import { VerifyLoggedInUser } from "../auth/token.js";
+import {
+  AddPost,
+  DeletePost,
+  FetchPost,
+  LoggedInUserRelatedPost,
+  RelatedUserPost,
+  SinglePost,
+  editPost,
+} from "../Controllers/post.Controller.js";
 
-export let postRouter = express.Router()
-postRouter.post('/', AddPost);
-postRouter.get('/', FetchPost);
-postRouter.get('/:id',SinglePost)
-postRouter.put('/:id', editPost)
-postRouter.get('/own/posts',LoggedInUserRelatedPost)
-postRouter.delete('/:id', DeletePost)
-postRouter.get('/relatedUserPost/:name', RelatedUserPost)
+export let postRouter = express.Router();
+postRouter.post("/", VerifyLoggedInUser, AddPost);
+postRouter.get("/", FetchPost);
+postRouter.get("/:id", SinglePost);
+postRouter.put("/:id", editPost);
+postRouter.get("/own/posts", LoggedInUserRelatedPost);
+postRouter.delete("/:id", DeletePost);
+postRouter.get("/relatedUserPost/:name", RelatedUserPost);
